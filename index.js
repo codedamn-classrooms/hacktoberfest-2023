92f6a07b06fed4952afac33b35fff07da7735399
Pranav <pranav.techiegeek@gmail.com>
2023-10-02 13:15:42 +0530

    ready

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+import axios from "axios";
 
-// Fix the Bug!
+/**
+ * returns a random joke from an api
+ * @returns {string} Joke
+ */
+export async function getJoke() {
+	// Obtain the response from API
+	const response = axios.get(
+		"https://official-joke-api.appspot.com/random_joke"
+	);
 
-const company = "Nike"
-const tagline = "Just do it"
+	// Parse the response
+	const data = response.data;
 
-[company, tagline].forEach(
-	(el) => console.log(el)
-);
+	return {
+		setup: data.setup,
+		punchline: data.punchline,
+	};
+}
+
+getJoke();
+
+// Sample Test
+const joke = await getJoke();
+console.log(joke);

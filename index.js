3bc4579085a3a27c230f39b9e6f6ba4ed96b5f84
Pranav <pranav.techiegeek@gmail.com>
2023-10-02 15:59:45 +0530

    ready

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const petName = "Leo";
-const placeholder = "{NAME}";
-const reminderTemplate =
-	"{NAME} is due for another visit. Please call us so we can set up a new appointment. We look forward to seeing you and {NAME} soon.";
-
-const reminder = reminderTemplate.replace(placeholder, petName);
-
+/**
+ * Returns a string with the pet name
+ * @param {string} petName
+ * @param {string} [placeholder= "{Name}"]
+ * @returns {string} Email String
+ */
+export function updateTemplateLiteral(petName, placeholder = "{NAME}") {
+	const reminderTemplate =
+		"{NAME} is due for another visit. Please call us so we can set up a new appointment. We look forward to seeing you and {NAME} soon.";
+	return reminderTemplate.replace(placeholder, petName);
+}
 // Test your code
-console.log(reminder);
+console.log(updateTemplateLiteral("Leo"));
+console.log(updateTemplateLiteral("Stella", "{NAME}"));

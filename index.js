91b2350f838cf45484f00fd361fdde27106d5917
Pranav <pranav.techiegeek@gmail.com>
2023-10-16 14:29:05 +0530

    ready

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PUBLIC_PORT;
 
-// Assume a simple data store
 const dataStore = [
 	{ id: 1, name: "Item 1" },
 	{ id: 2, name: "Item 2" },
-	// ...
 ];
 
 app.get("/get-data", (req, res) => {

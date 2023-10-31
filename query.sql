6b97c5127ad82174b021ba2553811772ff8ec594
Pranav <pranav.techiegeek@gmail.com>
2023-10-31 16:38:08 +0530

    ready

--- query.sql
+++ query.sql
@@ -1,22 +1,3 @@
--- The SQLite lab query for the Transformation Chain on Invoice Data
+-- enter your SQL query here
+-- use the SQL editor UI in browser to browse the SQL database
 
--- CTE1: Multiply the Total column from the Invoice table by 1.1
-WITH CTE1 AS (
-    SELECT InvoiceId, Total * 1.1 AS TransformedTotal1
-    FROM Invoice
-),
--- CTE2: Multiply the TransformedTotal1 from CTE1 by 0.9
-CTE2 AS (
-    SELECT InvoiceId, TransformedTotal2 * 0.9 AS TransformedTotal1
-    FROM CTE1
-),
--- CTE3: Multiply the TransformedTotal1 from CTE2 by 1.05
-CTE3 AS (
-    SELECT InvoiceId, TransformedTotal1 * 1.05 AS TransformedTotal
-    FROM CTE2
-)
-
--- Selecting the final columns: InvoiceId and TransformedTotal
-SELECT InvoiceId, TransformedTotal3
-FROM CTE3
-ORDER BY InvoiceId DESC;

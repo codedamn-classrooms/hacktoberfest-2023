769c69756711253b5b82a086578b78dd89f87caa
Pranav <pranav.techiegeek@gmail.com>
2023-10-31 16:40:11 +0530

    ready

--- query.sql
+++ query.sql
@@ -1,3 +1,18 @@
--- enter your SQL query here
--- use the SQL editor UI in browser to browse the SQL database
+-- Running Total of Invoices per Customer
 
+WITH InvoiceJoin AS (
+    SELECT 
+        i1."CustomerId",
+        i1."InvoiceDate" AS "InvoiceDate",
+        SUM(i1."Total") AS "RunningTotal"
+    FROM "Invoice" i1
+    JOIN "Invoice" i2 ON i1."CustomerId" = i2."CustomerId" 
+    AND i2."InvoiceDate" >= i1."InvoiceDate" 
+    GROUP BY i1."CustomerId", i1."InvoiceDate", i1."Total" 
+)
+
+SELECT 
+    "CustomerId", 
+    "InvoiceDate", 
+    "RunningTotal"
+FROM InvoiceJoin; 

f9586d85ce9eef786311e13fdca480fc1b74ae04
Pranav <pranav.techiegeek@gmail.com>
2023-10-31 22:41:55 +0530

    ready

--- query.sql
+++ query.sql
@@ -1,12 +1,16 @@
 -- enter your SQL query here
 -- use the SQL editor UI in browser to browse the SQL database
 
-SELECT 
-    CustomerId, 
-    InvoiceDate, 
-    julianday(InvoiceDate) - julianday(LAG(InvoiceDate) OVER (PARTITION BY CustomerId ORDER BY julianday(InvoiceDate))) AS DaysSinceLastInvoice
+SELECT
+    i."CustomerId",
+    MAX(i."InvoiceDate") AS "MaxInvoiceDate"
 FROM 
-    Invoice
+    "Customer" c
+LEFT JOIN
+    "Invoice" i
+ON 
+    c."CustomerId" = i."CustomerId"
+GROUP BY 
+    i."CustomerId"
 ORDER BY 
-    CustomerId ASC,
-    julianday(InvoiceDate) DESC;
+    c."CustomerId" ASC;
